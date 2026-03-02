8a2077c7508ffe4ab565a4717c24ffedc0677dfd
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-03-02 12:26:28 +0700

    first commit

---
+++ FunctionWithParameter2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void input()
+{
+    cout << "Masukan Panjang :";
+    cin >> panjang;
+    cout << "Masukan lebar";
+    cin >> lebar;
+
+}
+
+
+
